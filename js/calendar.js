2b4bee37ed110a0a1822da067564cb308b5a90ee
vitaliy-chervyak <vitaliychervyak23@gmail.com>
2019-09-04 15:17:40 +0300

    Updates

---
+++ js/calendar.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('#calendar-1').datetimepicker({
+        timepicker: false,
+        format:'d-m-Y',
+        defaultDate: '11-02-2016',
+    })
+    $('#calendar-2').datetimepicker({
+        timepicker: false,
+        format:'d-m-Y',
+        defaultDate: '11-06-2016',
+    })
+
+})
+    
